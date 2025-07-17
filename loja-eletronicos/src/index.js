0ccfa759ae8f3832099422dbaa18faed2ad86164
Eduarda Vitor <eduardavitor5@gmail.com>
2025-07-17 20:00:07 -0300

    pagina cadastro

--- loja-eletronicos/src/index.js
+++ loja-eletronicos/src/index.js
@@ -1,12 +1,25 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HomePage from './pages/HomePage'; // Importe a HomePage
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from './pages/HomePage';
+import CreateProductPage from './pages/CreateProductPage'; // Vamos criar esta página a seguir
 import './index.css';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/cadastrar",
+    element: <CreateProductPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HomePage />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
